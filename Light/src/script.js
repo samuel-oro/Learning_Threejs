d3aefb0a3d32eec3f0a24542eee0794f07a43147
samuel-oro <samuel@oroborus.in>
2026-02-19 16:48:33 +0530

    light and shadow

--- Light/src/script.js
+++ Light/src/script.js
@@ -22,16 +22,37 @@ scene.add(ambientlight)
 gui.addColor(ambientlight, 'color').name('Ambient Color')
 
 const spotLight = new THREE.SpotLight(0x78ff00, 0.5, 6, Math.PI * 0.1,0.25, 1)
-spotLight.position.x = 0.9
+spotLight.position.set(0, 2, 2)
 gui.add(spotLight, 'intensity').min(0).max(5).step(0.001).name('Spot Intensity')
 gui.addColor(spotLight, 'color').name('Spot Color')
 scene.add(spotLight)
+scene.add(spotLight.target)
+
+spotLight.castShadow = true
+spotLight.shadow.mapSize.set(1024, 1024)
+spotLight.shadow.camera.near = 0.5
+spotLight.shadow.camera.far = 10
+spotLight.shadow.bias = -0.0005
+spotLight.shadow.normalBias = 0.02
+spotLight.shadow.radius = 4
 
 const directionalLight = new THREE.DirectionalLight('red', 2)
 directionalLight.position.set(1, 1, 0)
 gui.add(directionalLight, 'intensity').min(0).max(5).step(0.001).name('Direct Intensity')
 gui.addColor(directionalLight, 'color').name('Direct Color')
 scene.add(directionalLight)
+
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024) 
+directionalLight.shadow.camera.near = 0.5
+directionalLight.shadow.camera.far = 10
+directionalLight.shadow.camera.left = -2
+directionalLight.shadow.camera.right = 2
+directionalLight.shadow.camera.top = 2
+directionalLight.shadow.camera.bottom = -2
+directionalLight.shadow.bias = -0.0005
+directionalLight.shadow.normalBias = 0.02
+directionalLight.shadow.radius = 4
 
 
 /**
@@ -46,23 +67,27 @@ const sphere = new THREE.Mesh(
     new THREE.SphereGeometry(0.5, 32, 32),
     material
 )
+sphere.castShadow = true
 sphere.position.x = - 1.5
 
 const cube = new THREE.Mesh(
     new THREE.BoxGeometry(0.75, 0.75, 0.75),
     material
 )
+cube.castShadow = true
 
 const torus = new THREE.Mesh(
     new THREE.TorusGeometry(0.3, 0.2, 32, 64),
     material
 )
+torus.castShadow = true
 torus.position.x = 1.5
 
 const plane = new THREE.Mesh(
     new THREE.PlaneGeometry(5, 5),
     material
 )
+plane.receiveShadow = true
 plane.rotation.x = - Math.PI * 0.5
 plane.position.y = - 0.65
 
@@ -89,6 +114,7 @@ window.addEventListener('resize', () =>
     // Update renderer
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
 })
 
 /**
@@ -113,7 +139,10 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.outputColorSpace = THREE.SRGBColorSpace
 
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
 /**
  * Animate
  */
